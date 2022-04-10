7db2b279d485b1475b40e78403043478bcd164eb
GabrielRaimundo123 <99844833+GabrielRaimundo123@users.noreply.github.com>
2022-04-10 19:23:35 -0300

    Create Resultado.php

---
+++ LIST_1/EX_5/Resultado.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Calculos</h1>
+
+    <?php
+      $valor1 = $_POST['valor1'];
+      $valor2 = $_POST['valor2'];
+      $valor2 = $_POST['valor3'];
+      $valor2 = $_POST['valor4'];
+      $valor2 = $_POST['valor5'];
+      echo "Valor 1 é:  $valor1, Valor 2 é:  $valor2, Valor 3 é: $valor3, Valor 4 é: valor4";
+      echo "<br/>";
+      if ($valor2 != 0){
+         echo "Divisão: ".($valor1 / $valor2);
+      } else {
+        echo "Não é possível realizar divisão por 0!";
+      }
+      echo $resultado;
+
+      $i = 10;
+
+
+    ?>
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
