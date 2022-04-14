a15899150498bd7df210b9fbc691ec32663db0f1
GabrielRaimundo123 <99844833+GabrielRaimundo123@users.noreply.github.com>
2022-04-13 22:56:43 -0300

    Update index.php

--- LIST_3/EX_5/index.php
+++ LIST_3/EX_5/index.php
@@ -8,34 +8,35 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercício 3</title>
+    <title>Exercício 5</title>
   </head>
   <body class="container">
     <h1>Divisores</h1>
 
-    <form action="Resposta.php" method="post">
-      <div class="row">
-        <?php
-          for ($i=1; $i<=5; $i++){
-        ?>
-
-          <div class="col">
-            <label for="nome<?= $i ?>" class="label-control">
-              Informe o valor <?= $i ?>
-            </label>
-            <input type="name" name="nome<?= $i ?>" 
-              id="nome<?= $i ?>" class="form-control"/>
-          </div>
-            
-        <?php
-          }
-        ?>
-      </div>
-      <div class="row">
-        <div class="col">
-          <button class="btn btn-primary">Enviar</button>
+    <form  action="Resposta.php" method="post">
+        <div class="row">
+            <div class="col">
+              <label for="altura" class="label-control">
+                Informe a altura:
+              </label>
+              <input type="number" name="altura" 
+                  id="altura" class="form-control"/>
+            </div>
+            <div class="col">
+            <label for="peso" class="label-control">
+                Informe o peso:
+              </label>
+              <input type="number" name="peso" 
+                  id="peso" class="form-control"/>
+            </div>
         </div>
-      </div>
+        <div class="row mt-5">
+          <div class="col">
+              <button type="submit" class="btn btn-danger">
+                Enviar
+              </button>
+          </div>
+        </div>
     </form>
     
     <!-- Optional JavaScript; choose one of the two! -->
