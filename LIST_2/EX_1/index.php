7afc10c98ddeb3dee1b455e6b5849442042c82a4
GabrielRaimundo123 <99844833+GabrielRaimundo123@users.noreply.github.com>
2022-04-13 23:52:36 -0300

    Update index.php

--- LIST_2/EX_1/index.php
+++ LIST_2/EX_1/index.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="pt-br">
   <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
@@ -8,31 +8,35 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <title>Exercício 1</title>
+    <title>Hello, world!</title>
   </head>
   <body class="container">
-    <h1>Exercício 1</h1>
-    <form action="resposta.php" method="post">
-      <div class="row">
-        <?php
-          for ($i=1; $i<=10; $i++){
-        ?>
-          <div class="col">
-            <label for="valor<?= $i ?>" class="label-control">
-              Informe o valor <?= $i ?>
-            </label>
-            <input type="number" name="valor<?= $i ?>" 
-              id="valor<?= $i ?>" class="form-control"/>
-          </div>
-        <?php
-          }
-        ?>
-      </div>
-      <div class="row">
-        <div class="col">
-          <button class="btn btn-primary">Enviar</button>
+    <h1>Exemplo de Envio de Formulário</h1>
+
+    <form method="post" action="Resposta.php">
+        <div class="row">
+            <div class="col">
+              <label for="valor1" class="label-control">
+                Informe o valor 1:
+              </label>
+              <input type="number" name="valor1" 
+                  id="valor1" class="form-control"/>
+            </div>
+            <div class="col">
+            <label for="valor2" class="label-control">
+                Informe o valor 2:
+              </label>
+              <input type="number" name="valor2" 
+                  id="valor2" class="form-control"/>
+            </div>
         </div>
-      </div>
+        <div class="row mt-5">
+          <div class="col">
+              <button type="submit" class="btn btn-danger">
+                Enviar
+              </button>
+          </div>
+        </div>
     </form>
 
     <!-- Optional JavaScript; choose one of the two! -->
