f37728ab1de8c9eb909a78042c838b097443a425
GabrielRaimundo123 <99844833+GabrielRaimundo123@users.noreply.github.com>
2022-04-13 22:48:36 -0300

    Update Resposta.php

--- LIST_3/EX_4_arumar_RE/Resposta.php
+++ LIST_3/EX_4_arumar_RE/Resposta.php
@@ -18,7 +18,7 @@
     $valor = 0;
     function soma($valor){
       $soma = 0;
-      for ($i= 1; $i< $valor); $i++){
+      for ($i= 1; $i< $valor; $i++){
         if($valor % $i == 0){
         $soma += $i;    
       } 
@@ -35,6 +35,7 @@
 
 
 <?php
+
 
 ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
